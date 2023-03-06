ac7eab901767af14048badaebddbd28b94c4f7de
kim-seonkyu <seonkyu93@gmail.com>
2023-03-06 22:47:18 +0900

    add / edit / complete / delete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRecoilValue } from "recoil";
+import "./App.css";
+import TodoItem from "./components/TodoItem";
+import TodoItemCreator from "./components/TodoItemCreator";
+import { todoListState } from "./TodoAtoms";
 
 function App() {
+  const todoList = useRecoilValue(todoListState);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoItemCreator />
+      {console.log(todoList)}
+      {todoList.map((todo) => (
+        <TodoItem key={todo.id} item={todo} />
+      ))}
     </div>
   );
 }
